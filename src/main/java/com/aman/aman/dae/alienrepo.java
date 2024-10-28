aece083117c4c05d75a8b14939cec059054a8790
amanchaudary <amanecdy@gmail.com>
2024-10-28 22:57:05 +0900

    code change

--- src/main/java/com/aman/aman/dae/alienrepo.java
+++ src/main/java/com/aman/aman/dae/alienrepo.java
@@ -1,10 +1,11 @@
 package com.aman.aman.dae;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.aman.aman.model.alien;
 
-// crudRepositary vitra same table ko data handle garinxa
-public interface alienrepo  extends  CrudRepository<alien,Integer>{
+@Repository
+public interface alienrepo extends CrudRepository<alien,Integer>{
 
 }
